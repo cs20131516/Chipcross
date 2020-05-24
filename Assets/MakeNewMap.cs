a6f4e10bd2ba4ff39499f7b70a61c677ea7b3956
JunHan <fightzuzak@naver.com>
2020-05-25 00:30:42 +0900

    아... 에반데///
    8,9타일 씁

--- Assets/MakeNewMap.cs
+++ Assets/MakeNewMap.cs
@@ -89,91 +89,6 @@ public class MakeNewMap : MonoBehaviour
         }
     }
 
-    public void makeNewlevel()
-    {
-        if(Newmap == null)//이부분 중요했음 ㅋㅋ 기존 맵 Destoryer
-        {
-
-        }
-        else
-        {
-            Newmap = null;
-        }
-        BoardWidth = Random.Range(3, 5);
-        BoardHeight = Random.Range(3, 5);
-        int scalesizechanger = 0;
-        scalesizechanger = (BoardWidth + BoardHeight) / 2;
-        switch (BoardHeight)
-        {
-            case 1:
-            case 2:
-            case 3:
-                scaleSize = 1;
-                break;
-            case 4:
-                scaleSize = 2;
-                break;
-            case 5:
-                scaleSize = 3;
-                break;
-        }
-        BoyPos = Random.Range(0, BoardHeight - 1);
-        GirlPos = Random.Range(0, BoardHeight - 1);
-        int tilevalue = 0;
-        int rangeoftile = 9;
-        bool tile8yes = true;//tile8yes 없애면 tile9 1개랑 나머지는 tile8개로 나옴
-        bool tile9yes = true;
-        for (int i = 0; i < BoardHeight; i++)
-            for (int j = 0; j < BoardWidth; j++)
-            {
-                tilevalue = Random.Range(1, rangeoftile);// 1부터
-                if (tilevalue == 8 && tile8yes)
-                {
-                    if (tile9yes)
-                    {
-                        tilevalue = 9;
-                        tile9yes = false;
-                    }
-                    else
-                    {
-                        tile8yes = false;
-                        rangeoftile = 8;
-                    }
-                }
-                //타일 Exception
-                if (j == 0)//<-
-                {
-                    while (tilevalue == 2)
-                    {
-                        tilevalue = Random.Range(1, rangeoftile);
-                    }
-                }
-                else if (i == 0)
-                {
-                    while (tilevalue == 5)
-                    {
-                        tilevalue = Random.Range(1, rangeoftile);
-                    }
-                }
-                else if (j == BoardHeight - 1)
-                {
-                    while (tilevalue == 4)
-                    {
-                        tilevalue = Random.Range(1, rangeoftile);
-                    }
-                }
-                else if (j == GirlPos && i == BoardWidth - 1)
-                {
-                    while (tilevalue == 2)
-                    {
-                        tilevalue = Random.Range(1, rangeoftile);
-                    }
-                }
-                Newmap += tilevalue;
-            }
-        BoardEmptyTileTypeInfo = ConvertStringToIntArray(Newmap); // 이걸 통해 만들어서 확인후에 돌아가는지 확인하고 pieces 나누기로 합시당.
-    }
-
     public void LoadLevelData(int num)
     {
         /*switch (num)
@@ -192,11 +107,8 @@ public class MakeNewMap : MonoBehaviour
                 makeNewlevel();
             break;
         }*/
-        makeNewlevel();
-        passivemaker();
-        //Checkingsame();
 
-        //Mapmakingbydfactor(-1);
+        Mapmakingbydfactor(-1);
     }
 
     string SetDefaultBoard()  //Return Default Board with all standard EmptyTiles
@@ -207,265 +119,6 @@ public class MakeNewMap : MonoBehaviour
             board += "1";
         }
         return board;
-    }
-
-    void Checkingsame()
-    {
-        if (check == Newmap)
-        {
-            if (GirlPos == Girlposcheck && BoyPos == Boyposcheck)
-                LoadLevelData(0);
-        }
-        else
-        {
-            check = Newmap;
-            Girlposcheck = GirlPos;
-            Boyposcheck = BoyPos;
-        }
-    }
-
-    void passivemaker()
-    {
-        float difficultyFactor = Random.Range(-1, 1);
-
-        if (Newmap == null)//이부분 중요했음 ㅋㅋ 기존 맵 Destoryer
-        {
-
-        }
-        else
-        {
-            Newmap = null;
-        }
-
-        if (passivemapcheck == passivemap)
-            passivemap++;
-
-        BoardWidth = 2; // 2*2타일같이 여기서 수정함
-        BoardHeight = 2;
-        scaleSize = 1;
-        //여자애 남자애 포지션
-        BoyPos = 0;
-        GirlPos = 0;
-
-        mapmatrix();
-
-        //Debug.Log(passivemap);
-
-        Newmap += passivemap;// 이 증가의 형태를 설명한다!
-        /* 0 0  -> 0 0 -> 0 0 -> 0 1
-           0 0     0 1    1 0    1 1 */
-
-        passivemapcheck = passivemap;
-        passivemap++;
-        /* 다음에 할거
-         Pin = 0000
-print (Pin)
-while Pin < 10000:
-    print (Pin)
-    Pin = Pin + 1
-         */
-        BoardEmptyTileTypeInfo = ConvertStringToIntArray(Newmap);
-    }
-
-    void mapmatrix()
-    {
-        //difficultyFactor가 -1~1 -> size스케일러
-        int Big = 0;
-        int sizescaler = 0;
-        sizescaler = (passivemapmatrix.GetLength(0) + passivemapmatrix.GetLength(1));
-        //ez case check
-        switch(sizescaler)
-        {
-            case 4:
-                difficultyFactor = -1;
-                break;
-            case 5:
-                difficultyFactor = -0.8f;
-                break;
-            case 6:
-                difficultyFactor = -0.5f;
-                break;
-            case 7:
-                difficultyFactor = -0.2f;
-                break;
-            case 8:
-                difficultyFactor = 0.1f;
-                break;
-            case 9:
-                difficultyFactor = 0.4f;
-                break;
-            case 10:
-                difficultyFactor = 0.7f;
-                break;
-        }
-
-        if (BoardWidth > BoardHeight)
-        {
-            Big = BoardWidth;
-        }
-        else
-        {
-            Big = BoardHeight;
-        }
-
-        passivemapmatrix[0, 0] = passivemap / 1000;
-        passivemapmatrix[0, 1] = (passivemap % 1000) / 100;
-        passivemapmatrix[1, 0] = (passivemap % 100) / 10;
-        passivemapmatrix[1, 1] = passivemap % 10;
-
-
-        /*Debug.Log("00:"+passivemapmatrix[0, 0]);
-        Debug.Log("01:" + passivemapmatrix[0, 1]);
-        Debug.Log("10:" + passivemapmatrix[1, 0]);
-        Debug.Log("11:" + passivemapmatrix[1, 1]);*/
-
-        int checkmatrixnum8 = 0;
-        int checkmatrixnum9 = 0;
-        //1천대
-        if (passivemapmatrix[0, 0] == 0)
-        {
-            passivemapmatrix[0, 0] += 1;
-        }
-        else if(passivemapmatrix[0, 0] == 8)
-        {
-            checkmatrixnum8++;
-        }
-        else if (passivemapmatrix[0, 0] == 9)
-        {
-            checkmatrixnum9++;
-        }
-        else if (passivemapmatrix[0, 0] == 6 || passivemapmatrix[0, 0] == 7)
-        {
-            difficultyFactor += 0.1f;
-        }
-
-        //Debug.Log("00:" + passivemapmatrix[0, 0]);
-
-        //1백대
-        if (passivemapmatrix[0, 1] == 0)
-        {
-            passivemapmatrix[0, 1] += 1;
-        }
-        else if (passivemapmatrix[0, 1] == 8)
-        {
-            checkmatrixnum8++;
-        }
-        else if (passivemapmatrix[0, 1] == 9)
-        {
-            checkmatrixnum9++;
-        }
-        else if (passivemapmatrix[0, 1] == 6 || passivemapmatrix[0, 1] == 7)
-        {
-            difficultyFactor += 0.1f;
-        }
-
-        //Debug.Log("01:" + passivemapmatrix[0, 1]);
-
-        //10대
-        if (passivemapmatrix[1, 0] == 0)
-        {
-            passivemapmatrix[1, 0] += 1;
-        }
-        else if (passivemapmatrix[1, 0] == 8)
-        {
-            checkmatrixnum8++;
-        }
-        else if (passivemapmatrix[1, 0] == 9)
-        {
-            checkmatrixnum9++;
-        }
-        else if (passivemapmatrix[1, 0] == 6 || passivemapmatrix[1, 0] == 7)
-        {
-            difficultyFactor += 0.1f;
-        }
-
-        //Debug.Log("10:" + passivemapmatrix[1, 0]);
-
-        //1대
-        if (passivemapmatrix[1, 1] == 0)
-        {
-            passivemapmatrix[1, 1] += 1;
-        }
-        else if (passivemapmatrix[1, 1] == 8)
-        {
-            checkmatrixnum8++;
-        }
-        else if (passivemapmatrix[1, 1] == 9)
-        {
-            checkmatrixnum9++;
-        }
-        else if (passivemapmatrix[1, 1] == 6 || passivemapmatrix[1, 1] == 7)
-        {
-            difficultyFactor += 0.1f;
-        }
-
-        //Debug.Log("11:" + passivemapmatrix[1, 1]);
-
-
-        // 확인
-        //Debug.Log("8:"+ checkmatrixnum8);
-        //Debug.Log("9:" + checkmatrixnum9);
-        if ((checkmatrixnum8 == 0 && checkmatrixnum9 == 0) || (checkmatrixnum8 == 1 && checkmatrixnum9==1))
-        {
-            Debug.Log("ok");
-            difficultyFactor += 0.05f;
-        }
-        else
-        {
-            Debug.Log("notok");
-            passivemap++;
-            mapmatrix();
-        }
-        
-        if (passivemapmatrix[0, 0] == 2 || passivemapmatrix[0, 0] == 6 || passivemapmatrix[0, 0] == 5)
-            passivemapmatrix[0, 0] += 1;
-        if (passivemapmatrix[0, 1] == 2 || passivemapmatrix[0, 1] == 6 || passivemapmatrix[0, 1] == 5)
-            passivemapmatrix[0, 1] += 1;
-        if (passivemapmatrix[1, 0]== 4 || passivemapmatrix[1, 0] == 7)
-            passivemapmatrix[1, 0] += 1;
-        if (passivemapmatrix[1, 1] == 4 || passivemapmatrix[1, 1] == 7)
-            passivemapmatrix[1, 1] += 1;
-
-        passivemap = passivemapmatrix[0, 0] * 1000 + passivemapmatrix[0, 1] * 100 + passivemapmatrix[1, 0] * 10 + passivemapmatrix[1, 1];
-
-        int k = 0;
-        for (int i = 0; i < BoardHeight; i++)
-        {
-            for (int j = 0; j < BoardWidth; j++)
-            {
-                if (passivemapmatrix[i, j] == 1)
-                    k++;
-            }
-        }
-
-        if(k==Big)
-        {
-            for (int i = 0; i < BoardHeight; i++)
-            {
-                for (int j = 0; j < BoardWidth; j++)
-                {
-                    if (passivemapmatrix[i, j] == 6 || passivemapmatrix[i, j] == 7)
-                        difficultyFactor = difficultyFactor + 0.06f;
-                }
-            }
-        }
-
-        if (difficultyFactor<-1)
-        {
-            difficultyFactor = -1;
-        }
-        else if(difficultyFactor>1)
-        {
-            difficultyFactor = 1;
-        }
-        /*
-        if (난이도 >= 0.5)
-        {
-            워프 타일 X
-            6,7번 타일 X
-            공백 타일 갯수 = 난이도 * 0.5f
-        }
-        */
     }
 
     void Mapmakingbydfactor(float dfactor)
@@ -600,18 +253,53 @@ while Pin < 10000:
         // levelfactor의 갯수들 8,9를 만들거면 최소 0.06필요
         // 6,7 대체의 경우는 0.05, 추가의경우는 1개당 0.07필요하다
 
-        int k = 0;
-
-        while (levelfactor>0)
+        int k = Random.Range(0, 1);
+        //levelfactor <=0.1 -> tile을 변경하는거지
+        if (levelfactor <=0.1)//tile 교체
         {
-            if(Big>0)// 대체를 먼저하는게 좋을까 아니면 추가를 먼저하는게 좋을까? 당연히 대체를 먼저하는게 이득이지
+            if(k==0)//6,7로 바꾸는거
             {
-                k = Random.Range(1, 3);
+                int i = 0;
+                for (; i < listoftile.Count; i++)
+                {
+                    listoftile[i] = Random.Range(2, 7);
+                }
             }
-            else//Big == 0
+            else//8,9로 바꾸는거
             {
+                int i = 0;
+                for (; i < listoftile.Count-2; i++)
+                {
+                    listoftile[i] = Random.Range(2, 7);
+                }
+                listoftile[i + 1] = 8;
+                listoftile[i + 1] = 9;
+            }
+        }
+        else if(levelfactor>0.1 && levelfactor<=0.2)//1개~(Boardh+w)*3/4-(Big+2) 추가가 적당할 거같음
+        {
+            if (k == 0)//6,7 추가 몇개를?
+            {
+                int i = 0;
+                for (; i < listoftile.Count; i++)
+                {
+                    listoftile[i] = Random.Range(2, 7);
+                }
+            }
+            else
+            {
+                int i = 0;
+                for (; i < listoftile.Count - 2; i++)
+                {
+                    listoftile[i] = Random.Range(2, 7);
+                }
+                listoftile[i + 1] = 8;
+                listoftile[i + 1] = 9;
+            }
+        }
+        else
+        {
 
-            }
         }
 
         k = 0;

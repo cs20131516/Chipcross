462d5a98ec97f077d5f9cb57be7277e09b7574af
JunHan <fightzuzak@naver.com>
2020-07-05 13:23:59 +0900

    12 시작
    Good

--- Assets/MakeNewMap.cs
+++ Assets/MakeNewMap.cs
@@ -245,7 +245,7 @@ public class MakeNewMap : MonoBehaviour
         scaleSize = 1;
         passivemapmatrix = new int[BoardHeight, BoardWidth];
         //여자애 남자애 포지션
-        BoyPos = 0;
+        BoyPos = 1;
         GirlPos = 2;
 
         //여기서 결정을 다 함.
@@ -313,12 +313,12 @@ while Pin < 10000:
         }
 
 
-        while (passivemapmatrix[BoyPos, 0] <= 2 || passivemapmatrix[BoyPos, 0] == 3 || passivemapmatrix[BoyPos, 0] == 5 || passivemapmatrix[BoyPos, 0] == 6 || (passivemapmatrix[BoyPos, 0] == 8 && passivemapmatrix[GirlPos, 1] == 9) || (passivemapmatrix[BoyPos, 0] == 9 && passivemapmatrix[GirlPos, 1] == 8)) //|| passivemapmatrix[GirlPos, 1] == 5 || passivemapmatrix[BoyPos, 0] == 6)
+        while (passivemapmatrix[BoyPos, 0] <= 2 || passivemapmatrix[BoyPos, 0] == 3 || (passivemapmatrix[BoyPos, 0] == 8 && passivemapmatrix[GirlPos, 1] == 9) || (passivemapmatrix[BoyPos, 0] == 9 && passivemapmatrix[GirlPos, 1] == 8)) //|| passivemapmatrix[GirlPos, 1] == 5 || passivemapmatrix[BoyPos, 0] == 6)
         {
             if (passivemapmatrix[BoyPos, 0] >= 9)
             {
                 passivemapmatrix[BoyPos, 0] = 4;
-                //passivemapmatrix[BoyPos - 1, 1] += 1; 0이라서 일단 없애놔도 된다
+                passivemapmatrix[BoyPos - 1, 1] += 1; //0이라서 일단 없애놔도 된다
             }
             else
             {
